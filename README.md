083ffd44dc9ff8ce68d962cae7bb091a48bc356b
マピオン <mapion@users.noreply.github.com>
2023-06-14 00:28:25 +0900

    first (#1)
    Co-authored-by: oc-honjo <honjyo@onecompath.com>

--- README.md
+++ README.md
@@ -1,1 +1,105 @@
 # world-precipitation-cog-map
+
+[![Deploy](https://github.com/mapion/world-precipitation-cog-map/actions/workflows/deploy.yml/badge.svg)](https://github.com/mapion/world-precipitation-cog-map/actions/workflows/deploy.yml)
+
+世界降水マップのCOG版
+
+<img width="394" alt="topic" src="mov.gif">
+
+## Architecture
+
+![architecture](architecture.drawio.svg)
+
+## Requirements
+
+Node.js 18 以上
+
+## Development
+
+> 以下はローカルマシンでの開発方法だが、Codespacesにも対応している
+
+ソースコードをローカルにcloneする
+
+```sh
+git clone https://github.com/mapion/world-precipitation-cog-map.git
+```
+
+リポジトリのディレクトリに移動
+
+```sh
+cd world-precipitation-cog-map
+```
+
+インストール
+
+```sh
+npm install
+```
+
+実行
+
+```sh
+npm run dev
+open http://localhost:3000
+```
+
+## Contributing
+
+1. Forkする
+
+2. コード修正
+
+    ```sh
+    vi index.html
+    vi src/main.ts
+    ```
+
+3. ビルドテスト
+
+    ```sh
+    npm run build
+    ```
+
+4. ビルドプレビュー
+
+    ```sh
+    npm run preview
+    open http://localhost:4173
+    ```
+
+5. プッシュ
+
+    ```sh
+    git add .
+    git commit -m "Update"
+    git push origin feature/xxx
+    ```
+
+6. プルリク作成
+
+## Release
+
+1. プルリクに対してレビューを実施
+2. プルリクをmainブランチにマージ
+
+    - Actionsが走り自動でGitHub Pagesにデプロイされる
+
+3. タグ作成
+
+    ```sh
+    git checkout main
+    git pull origin main
+    git tag v1.0.0
+    git push origin v1.0.0
+    ```
+
+4. GitHub上でリリースを作成
+
+    - タグを選択
+    - `Create release from tag`を押下
+    - `Genarate release notes`を押下
+    - `Publish release`を押下
+
+## License
+
+[MIT License](LICENSE)
---
+++ src/main.ts
@@ -0,0 +1,178 @@
+import maplibregl, { Map, RasterSourceSpecification } from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import { Pool, fromUrl } from 'geotiff';
+import { encode } from 'fast-png';
+
+function getTargetDate(): Date {
+    // 9時半を堺に対象の日付が変わる
+    // 9時半前なら前々日
+    // 9時半後なら前日
+    const now = new Date();
+    now.setHours(now.getHours() - 9);
+    now.setMinutes(now.getMinutes() - 30);
+    now.setDate(now.getDate() - 1);
+
+    return now;
+}
+const targetDate = getTargetDate();
+// console.log('targetDate', targetDate);
+
+function formatTargetDate(targetDate: Date): string {
+    const y = targetDate.getFullYear();
+    const m = ('00' + (targetDate.getMonth()+1)).slice(-2);
+    const d = ('00' + targetDate.getDate()).slice(-2);
+    const ret = y + '' + m + '' + d;
+    return ret;
+}
+const targetDateFormatted = formatTargetDate(targetDate);
+// console.log('targetDateFormatted', targetDateFormatted);
+
+function getVizDate(targetDate: Date): string {
+    targetDate.setDate(targetDate.getDate() + 1);
+    const y = targetDate.getFullYear();
+    const m = ('00' + (targetDate.getMonth()+1)).slice(-2);
+    const d = ('00' + targetDate.getDate()).slice(-2);
+    const ret = y + '/' + m + '/' + d;
+    return ret;
+}
+const vizDate = getVizDate(targetDate);
+// console.log('vizDate', vizDate);
+
+const headerTitle = document.getElementById('header-title') as HTMLElement;
+headerTitle.innerHTML = `${vizDate}&nbsp;3時の予報`;
+
+/**
+ * transform x/y/z to webmercator-bbox
+ * @param x
+ * @param y
+ * @param z
+ * @returns {number[]} [minx, miny, maxx, maxy]
+ */
+function merc(x: number, y: number, z: number): number[] {
+    // 参考: https://qiita.com/MALORGIS/items/1a9114dd090e5b891bf7
+    const GEO_R = 6378137;
+    const orgX = -1 * ((2 * GEO_R * Math.PI) / 2);
+    const orgY = (2 * GEO_R * Math.PI) / 2;
+    const unit = (2 * GEO_R * Math.PI) / Math.pow(2, z);
+    const minx = orgX + x * unit;
+    const maxx = orgX + (x + 1) * unit;
+    const miny = orgY - (y + 1) * unit;
+    const maxy = orgY - y * unit;
+    return [minx, miny, maxx, maxy];
+}
+
+// 参考: https://qiita.com/Kanahiro/items/70b3b8b11bd26cbaf30e
+const generateCogSource = async (
+    url: string,
+    id: number,
+): Promise<{ source: RasterSourceSpecification }> => {
+    const tiff = await fromUrl(url);
+    const pool = new Pool();
+    maplibregl.addProtocol(`cog${id}`, (params, callback) => {
+        const segments = params.url.split('/');
+        const [z, x, y] = segments.slice(segments.length - 3).map(Number);
+        const bbox = merc(x, y, z);
+        const size = 256;
+        tiff.readRasters({
+            bbox,
+            samples: [0, 1, 2, 3], // 取得するバンドを指定
+            width: size,
+            height: size,
+            interleave: true,
+            pool,
+        }).then((data) => {
+            const img = new ImageData(
+                //@ts-ignore
+                new Uint8ClampedArray(data),
+                size,
+                size,
+            );
+            const png = encode(img);
+            callback(null, png, null, null);
+        });
+        return { cancel: () => {} };
+    });
+    const source: RasterSourceSpecification = {
+        type: 'raster',
+        tiles: [`cog${id}://${url.split('://')[1]}/{z}/{x}/{y}`],
+        tileSize: 512,
+        minzoom: 1,
+        maxzoom: 11,
+        attribution: '© ONE COMPATH',
+    };
+    return { source };
+};
+
+const map = new Map({
+    container: 'map',
+    center: [139.036, 36.685],
+    zoom: 1,
+    minZoom: 1,
+    maxZoom: 11,
+    style: './style.json',
+    hash: true,
+});
+map.addControl(new maplibregl.NavigationControl());
+
+async function addWeather(id: number): Promise<void> {
+    const idZero = ('000' + id).slice(-3);
+    const cogPath = `https://mapion-vt-public-stg.s3.ap-northeast-1.amazonaws.com/lab/noaa/apcp/${targetDateFormatted}/18/${idZero}.tif`;
+    // console.log('cogPath', cogPath);
+    const { source } = await generateCogSource(cogPath, id);
+    const sourceId = `weather-cog-${id}`;
+    const layerId = `weather-cog-layer-${id}`;
+    // console.log('layerId', layerId);
+    map.addSource(sourceId, source);
+    map.addLayer({
+        id: layerId,
+        type: 'raster',
+        source: sourceId,
+        layout: {
+            visibility: 'none',
+        },
+        paint: { 'raster-opacity': 0.7 },
+    });
+}
+
+function switchWeather(id: number): void {
+    const fourecastDate = document.getElementById('forecast-date') as HTMLElement;
+    fourecastDate.textContent = `${id}時間後`;
+    for (let i = 6; i <= 72; i+=6) {
+        map.setLayoutProperty(`weather-cog-layer-${i}`, 'visibility', 'none');
+    }
+    map.setLayoutProperty(`weather-cog-layer-${id}`, 'visibility', 'visible');
+}
+
+map.on('load', async () => {
+    for (let i = 6; i <= 72; i+=6) {
+        await addWeather(i);
+    }
+    switchWeather(6);
+});
+
+const slider = document.getElementById('slider') as HTMLElement;
+slider.addEventListener('input', (e) => {
+    const target = e.target as HTMLInputElement;
+    const forecastTime = parseInt(target.value, 10);
+    switchWeather(forecastTime);
+});
+
+function popupImage(): void {
+    const popup = document.getElementById('js-popup') as HTMLElement;
+    if(!popup) return;
+
+    const blackBg = document.getElementById('js-black-bg') as HTMLElement;
+    const closeBtn = document.getElementById('js-close-btn') as HTMLElement;
+    const showBtn = document.getElementById('js-show-popup') as HTMLElement;
+
+    closePopUp(blackBg);
+    closePopUp(closeBtn);
+    closePopUp(showBtn);
+    function closePopUp(elem: HTMLElement): void {
+        if(!elem) return;
+        elem.addEventListener('click', function() {
+            popup.classList.toggle('is-show');
+        });
+    }
+}
+popupImage();
